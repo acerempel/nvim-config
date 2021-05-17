f95b75dff9da86b33702736a25adb37918c4e0c9
Alan Rempel <alan.rempel@gmail.com>
2021-05-16 21:52:40 -0400

    Add hover keybinding

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,13 @@
 local lspconfig = require('lspconfig')
 local util = require('lspconfig.util')
 
+local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
 lspconfig.intelephense.setup {
-  root_dir = util.root_pattern("composer.json")
+  root_dir = util.root_pattern("composer.json"),
+  on_attach = on_attach,
 }
