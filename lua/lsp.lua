ca1e30f3d85620d582b0fda2879ea0bc66e90cca
Alan Rempel <alan.rempel@gmail.com>
2021-05-17 13:37:31 -0400

    Add mapping for go-to-definition

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,6 +5,7 @@ local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
 end
 
 lspconfig.intelephense.setup {
