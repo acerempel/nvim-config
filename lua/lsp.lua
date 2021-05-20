55e642db83b49a1e9182203c31ca12623453509a
Alan Rempel <alan.rempel@gmail.com>
2021-05-20 09:34:06 -0400

    Tweak LSP settings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,14 +1,14 @@
 local lspconfig = require('lspconfig')
 local util = require('lspconfig.util')
 
-lsp_saga = require('lspsaga')
-lsp_hover = require('lspsaga.hover')
-lsp_provider = require('lspsaga.provider')
+local lsp_saga = require('lspsaga')
+local lsp_hover = require('lspsaga.hover')
+local lsp_provider = require('lspsaga.provider')
 
 lsp_saga.init_lsp_saga {
   code_action_prompt = { enable = false },
   finder_action_keys = {
-    open = "o", split = "s",
+    open = "<CR>", split = "s",
     vsplit = "v", quit = 'q',
     scroll_down = '<C-e>', scroll_up = '<C-y>',
   }
@@ -25,23 +25,23 @@ local lsp_signature_config = {
 
 local on_attach = function(client, bufnr)
   lsp_signature.on_attach(lsp_signature_config)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'K', "<Cmd>lua require'lspsaga.hover'.render_hover_doc()<CR>", opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<Leader>ji', "<Cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", opts)
-  buf_set_keymap('n', '<Leader>jp', "<Cmd>lua require'lspsaga.provider'.preview_definition()<CR>", opts)
-  buf_set_keymap('n', '<C-e>', "<Cmd>lua require'lspsaga.action'.smart_scroll_with_saga(1)<CR>", opts)
-  buf_set_keymap('n', '<C-y>', "<Cmd>lua require'lspsaga.action'.smart_scroll_with_saga(-1)<CR>", opts)
-  buf_set_keymap('n', '[w', "<Cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", opts)
-  buf_set_keymap('n', ']w', "<Cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", opts)
+  local function buf_set_keymap(mode, lhs, rhs)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+  end
+  buf_set_keymap('n', 'K', "<Cmd>lua require'lspsaga.hover'.render_hover_doc()<CR>")
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
+  buf_set_keymap('n', '<Leader>ji', "<Cmd>lua require'lspsaga.provider'.lsp_finder()<CR>")
+  buf_set_keymap('n', '<Leader>jp', "<Cmd>lua require'lspsaga.provider'.preview_definition()<CR>")
+  buf_set_keymap('n', '<C-e>', "<Cmd>lua require'lspsaga.action'.smart_scroll_with_saga(1)<CR>")
+  buf_set_keymap('n', '<C-y>', "<Cmd>lua require'lspsaga.action'.smart_scroll_with_saga(-1)<CR>")
+  buf_set_keymap('n', '[w', "<Cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>")
+  buf_set_keymap('n', ']w', "<Cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>")
 end
 
 lspconfig.intelephense.setup {
   root_dir = util.root_pattern("composer.json"),
   on_attach = on_attach,
 }
-
 lspconfig.rust_analyzer.setup { on_attach = on_attach }
-
 lspconfig.tsserver.setup { on_attach = on_attach }
