f9a397af31d7c203f81610327ea1a6c2a7eae239
Alan Rempel <alan.rempel@gmail.com>
2021-10-25 12:27:50 -0400

    Change mapping for textsubjects

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -78,7 +78,7 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     keymaps = {
       [';'] = 'textsubjects-smart',
-      ['.'] = 'textsubjects-container-outer',
+      [','] = 'textsubjects-container-outer',
     }
   },
 }
