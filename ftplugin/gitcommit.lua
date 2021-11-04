c02c5b3a417e9d196d359ca0b23c75c0af6d96e9
Alan Rempel <alan.rempel@gmail.com>
2021-11-03 23:41:53 -0400

    What not

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -1,7 +1,9 @@
 -- Better textwidth – override builtin ftplugin
 vim.opt_local.textwidth = 67
 api = vim.api
-if api.nvim_win_get_position(0)[1] > 1 then
-  buf_height = vim.fn.getbufinfo(0).linecount
-  api.nvim_win_set_height(0, buf_height)
-end
+-- if api.nvim_win_get_position(0)[1] > 1 then
+--   buf_height = vim.fn.getbufinfo(0).linecount
+--   api.nvim_win_set_height(0, buf_height)
+-- end
+api.nvim_command 'startinsert'
+api.nvim_buf_set_keymap(0, 'n', 'q', '<Cmd>wq<CR>', { noremap = true })
