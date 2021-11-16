bba4b6e7e8c8ee9de08cc0dc991ae46c4f4041b4
Alan Rempel <alan.rempel@gmail.com>
2021-11-16 14:28:11 -0500

    Ensure Vimwiki sees configuration before it is loaded

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -407,7 +407,35 @@ use {
 
 use {
   'vimwiki/vimwiki',
-  opt = true
+  event = { "BufReadPost,BufNew *.wiki" },
+  setup = function ()
+    vim.cmd [[
+      let g:vimwiki_list =
+        \ [{'path': '~/Documents/Grand-Schemes',
+        \   'path_html': '~/Documents/Grand-Schemes/Hypertext',
+        \   'index': 'Index',
+        \   'auto_toc': 1,
+        \   'syntax': 'default',
+        \   'ext': '.wiki',
+        \   'template_path': '~/Documents/Grand-Schemes/Templates',
+        \   'template_default': 'default',
+        \   'template_ext': '.template.html',
+        \   'diary_rel_path': 'Catalogue-of-Days',
+        \   'diary_index': 'Days',
+        \   'diary_header': 'The Catalogue of Days',
+        \   'diary_sort': 'desc',
+        \   'auto_tags': 1,
+        \   'auto_diary_index': 1,
+        \ }]
+
+      let g:vimwiki_dir_link = 'Index'
+      let g:vimwiki_html_header_numbering = 2
+      let g:vimwiki_html_header_numbering_sym = '.'
+      let g:vimwiki_autowriteall = 0
+      let g:vimwiki_ext2syntax = {}
+      let g:vimwiki_global_ext = 0
+    ]]
+  end
 }
 
 -- Syntax highlighting and suchlike
