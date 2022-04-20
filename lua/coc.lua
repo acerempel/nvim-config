eaf7b011a652f201479f03d4328e88f691aa7dcc
Alan Rempel <alan.rempel@gmail.com>
2022-04-20 10:03:30 -0400

    Coc better mappings initialization

--- lua/coc.lua
+++ lua/coc.lua
@@ -25,8 +25,6 @@ M.s_tab = function ()
 end
 
 M.coc_map_config = {
-  { 'K', 'doHover', 'Show documentation' },
-  { 'gd', 'jumpDefinition', 'Go to definition' },
   { 'go', '<Cmd>lua require("telescope").extensions.coc.document_symbols{}', 'Search document symbols' },
   { 'gO', 'showOutline', 'Show document outline' },
   { '<D-k>', 'definitionHover', 'Show definition' },
@@ -40,22 +38,13 @@ M.coc_map_config = {
   { '<Leader>dl', 'diagnosticList', 'List all diagnostics' },
 }
 
---[[ local filetypes = {}
+M.coc_buf_maps = function ()
+  local bufnr = vim.api.nvim_get_current_buf()
 
-M.coc_update_fts = function ()
-  local services = vim.fn.CocAction('services')
-  for _, service in ipairs(services) do
-    print(vim.inspect(service.languageIds))
-    for _, ft in ipairs(service.languageIds) do
-      filetypes[ft] = true
-    end
-  end
-end ]]
-
-M.coc_buf_maps = function (bufnr)
-  if not vim.api.nvim_buf_call(bufnr, function() return vim.fn.CocAction('ensureDocument') end) then
+  if not vim.fn.CocAction('ensureDocument') then
     return nil
   end
+
   local whichkey = require('which-key')
   local mappings = {}
   for _, map in ipairs(M.coc_map_config) do
@@ -79,18 +68,31 @@ M.coc_buf_maps = function (bufnr)
   end
   whichkey.register(mappings, { buffer = bufnr })
   whichkey.register(mappings, { buffer = bufnr, mode = 'v' })
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'z=', '<Plug>(coc-codeaction-cursor)', {})
-  vim.api.nvim_buf_set_keymap(bufnr, 'x', 'z=', '<Plug>(coc-codeaction-selected)', {})
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<S-D-.>', '<Plug>(coc-fix-current)', {})
+
+  local map_opts =  { silent = true, buffer = bufnr }
+
+  if vim.fn.CocHasProvider('hover') then
+    vim.keymap.set({'n', 'x'}, 'K', "<Cmd>call CocActionAsync('doHover')", map_opts)
+  end
+
+  if vim.fn.CocHasProvider('definition') then
+    vim.keymap.set({'n', 'x'}, 'gd', "<Cmd>call CocActionAsync('jumpDefinition')", map_opts)
+  end
+
+  if vim.fn.CocHasProvider('codeAction') then
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'z=', '<Plug>(coc-codeaction-cursor)', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'x', 'z=', '<Plug>(coc-codeaction-selected)', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<M-D-.>', '<Plug>(coc-fix-current)', {})
+  end
+
   vim.api.nvim_buf_set_keymap(bufnr, 'n', ']g', '<Plug>(coc-diagnostic-next)', {})
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '[g', '<Plug>(coc-diagnostic-prev)', {})
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<D-CR>', "<Cmd>call CocActionAsync('codeLensAction')<CR>", { noremap = true })
+
+  if vim.fn.CocHasProvider('codeLens') then
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<D-CR>', "<Cmd>call CocActionAsync('codeLensAction')<CR>", { noremap = true })
+  end
 
   vim.cmd [[
-  " inoremap <silent><expr> <TAB>
-  "       \ pumvisible() ? "\<C-n>" :
-  "       \ v:lua.require'util'.check_back_space() ? "\<TAB>" :
-  "       \ coc#refresh()
   inoremap <silent><buffer><expr> <Tab> luaeval("require('coc').tab()")
   inoremap <silent><buffer><expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<S-Tab>"
   inoremap <silent><buffer><expr> <C-Space> coc#refresh()
@@ -106,39 +108,33 @@ M.coc_buf_maps = function (bufnr)
                                 \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
   ]]
 
-  vim.api.nvim_buf_set_option(bufnr, 'formatexpr', "CocActionAsync('formatSelected')")
+  if vim.fn.CocHasProvider('documentHighlight') then
+    local highlight_autocmd = string.format(
+      "autocmd coc_highlight CursorHold <buffer=%d> silent call CocActionAsync('highlight')",
+      bufnr
+    )
+    vim.api.nvim_command(highlight_autocmd)
+  end
 
-  vim.api.nvim_buf_set_var(bufnr, 'coc_did_mappings', true)
+  if vim.fn.CocHasProvider('formatRange') then
+    vim.api.nvim_buf_set_option(bufnr, 'formatexpr', "CocActionAsync('formatSelected')")
+  end
 end
 
-local command = vim.api.nvim_command
-
 M.setup_coc_maps = function()
-  local extensions = vim.fn.CocAction('extensionStats')
-  local filetypes = { haskell = true }
-  for _, ext in ipairs(extensions) do
-    for _, event in ipairs(ext.packageJSON.activationEvents) do
-      local ft_match = event:match('^onLanguage:([%w%._-]+)$')
-      if ft_match then
-        filetypes[ft_match] = true
-      elseif ext.id == 'coc-html' then
-        filetypes['html'] = true
-      end
-    end
-  end
-  local fts_pat = table.concat(vim.tbl_keys(filetypes), ',')
-  command('augroup coc_buf_config')
-  command('au!')
-  local fts_autocmd = [[au FileType %s call luaeval('require("coc").coc_buf_maps(_A+0)', expand('<abuf>'))]]
-  command(string.format(fts_autocmd, fts_pat))
-  command([[autocmd CursorHold * silent call CocActionAsync('highlight')]])
-  command('augroup END')
+  vim.api.nvim_command('augroup coc_config')
+  vim.api.nvim_command('au!')
+  vim.api.nvim_command [[au FileType * lua require("coc").coc_buf_maps()]]
+  vim.api.nvim_command('augroup END')
+  vim.cmd [[
+    augroup coc_highlight
+    au!
+    augroup END
+  ]]
+
   local bufs = vim.api.nvim_list_bufs()
   for _, buf in ipairs(bufs) do
-    local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
-    if filetypes[ft] then
-      M.coc_buf_maps(buf)
-    end
+    vim.api.nvim_buf_call(buf, M.coc_buf_maps)
   end
 end
 
