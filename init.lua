a34622a5c3cbc3485fc01784b437f00e2dcdae2b
Alan Rempel <alan.rempel@gmail.com>
2023-11-15 23:37:35 -0500

    Screen line jk

--- init.lua
+++ init.lua
@@ -76,6 +76,10 @@ let g:maplocalleader = '\'
 
 -- Mappings {{{
 vim.cmd [[
+
+noremap <silent> <expr> j (v:count == 0 ? 'gj' : 'j')
+noremap <silent> <expr> k (v:count == 0 ? 'gk' : 'k')
+
 " Nomodifiable {{{
 augroup nomodifiable
   au!
